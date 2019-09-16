b745de27e75afa7115300c1308435c05aac97349
Leozera <leobhal13@gmail.com>
2019-09-16 04:43:14 -0300

    Advanced template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,36 +1,37 @@
 const path = require('path')
 const express = require('express');
-
+const hbs = require('hbs');
 //Define path for express config
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-const viwesPath = path.join(__dirname, '../templates');
+const viwesPath = path.join(__dirname, '../templates/views');
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viwesPath);
-
+hbs.registerPartials(partialsPath);
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Leonardo Ramos'
+        name: 'Leozera D3V!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
-        name: 'Leozera Dev!'
+        name: 'Leozera D3V!'
     });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Weather',
+        title: 'Help',
         TheName: 'Jesus Christ',
         name: 'Leozera D3V!'
     })
